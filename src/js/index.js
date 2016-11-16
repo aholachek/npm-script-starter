063cacfd6467b44d2d79c28004f870212e4acedd
Alex Holachek <aholachek@gmail.com>
2016-11-16 08:30:34 -0500

    server directory change

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
-/*
-ES6 is supported
- */
+//ES6 is supported
+import obj from './another-import';
+//importing from node_modules doesn't require a path
+import $ from 'jquery';
+
+console.log('Testing imports:', obj, $);
